124757b645d36fd68a613725ab74ffe9314ced41
Gichimu <gichimueric12@gmail.com>
2019-12-15 19:53:20 +0300

    add the image, location and category models and the initial migration

--- pics/models.py
+++ pics/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
 class Image(models.Model):
-    
+    image = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.image
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
