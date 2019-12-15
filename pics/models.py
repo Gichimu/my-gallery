5bf5ff91b0964451d207ea1358cfe2f80c3ee324
Gichimu <gichimueric12@gmail.com>
2019-12-15 20:09:55 +0300

    add the timestamp to the image model

--- pics/models.py
+++ pics/models.py
@@ -1,15 +1,5 @@
 from django.db import models
-
-class Image(models.Model):
-    image = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
-
-    def __str__(self):
-        return self.image
+from django.utils import timezone
 
 class Location(models.Model):
     name = models.CharField(max_length=50)
@@ -22,4 +12,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    image = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, default='')
+    category = models.ForeignKey(Category, default='')
+
+    def __str__(self):
+        return self.image
 
