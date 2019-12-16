0c895e62d47c26f8beb365341e7d282ee90e2e0b
Gichimu <gichimueric12@gmail.com>
2019-12-16 08:52:27 +0300

    heroku deployment run 2

--- gallery/settings.py
+++ gallery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MODE = config("MODE", default="dev")
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default="342s(s(!hsjd998sde8$=o4$3m!(o+kce2^97kp6#ujhi")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
